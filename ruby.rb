77c89df65abc95653a2c2800c43a009bf7dc35e5
emilyjf <flanagan.emily.j@gmail.com>
2017-08-11 17:16:48 -0500

    more ruby practice

--- ruby.rb
+++ ruby.rb
@@ -27,3 +27,8 @@ def array_copy(source)
   end
   return destination
 end
+
+def palindrome?(sentence)
+  downcase_stripped_sentence = sentence.downcase.gsub(" ", "")
+  downcase_stripped_sentence  ==  downcase_stripped_sentence.reverse
+end
