829c442956c54b404a4cb265cebe8f3d5cd886bf
emilyjf <flanagan.emily.j@gmail.com>
2017-10-01 21:11:27 -0500

    more ruby practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -179,18 +179,24 @@
 # Implement FizzBuzz. Write a program that prints the integers from 1 to 100. But for multiples of three print “Fizz” instead of the number, and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
 
-  (1..100).each do |num|
-    if num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    elsif num % 3 == 0 
-      puts "Fizz"
-    else
-      puts num
-    end
-  end
+  # (1..100).each do |num|
+  #   if num % 3 == 0 && num % 5 == 0
+  #     puts "FizzBuzz"
+  #   elsif num % 5 == 0
+  #     puts "Buzz"
+  #   elsif num % 3 == 0 
+  #     puts "Fizz"
+  #   else
+  #     puts num
+  #   end
+  # end
 
-  
-  
+N = gets.strip.to_i
 
+if N % 2 == 1 
+    print "Weird"
+elsif N % 2 == 0 && (N > 1 && N < 6) || (N > 20)
+    print "Not Weird"
+else N % 2 == 0 && N == 6..20
+    print "Weird"
+end
