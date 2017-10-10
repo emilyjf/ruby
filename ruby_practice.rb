2398f704340f4be11d4e58e91d993d2bc95d0a2c
emilyjf <flanagan.emily.j@gmail.com>
2017-10-09 20:58:21 -0500

    ruby practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -51,10 +51,10 @@
 
 # hash = {dog: 'Nicki', cat: 'Peanut', cat: 'Zadie'}
 # hash = {:dog => 'Nicki', :cat => 'Peanut', :cat => 'Zadie'}
-
-# Suppose you have a hash h = {a:1, b:2, c:3, d:4}
+# Suppose you have a hash 
+h = {a:1, b:2, c:3, d:4}
 # 1. Get the value of key `:b`.
-
+puts h['b']
 # 2. Add to this hash the key:value pair `{e:5}`
 
 # 3. Remove all key:value pairs whose value is less than 3.5
